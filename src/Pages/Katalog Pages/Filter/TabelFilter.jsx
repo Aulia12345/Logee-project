8568f6b8062c526a5a13c475e89457afc82841e8
lutfirz <lutfirazan@gmail.com>
2024-09-23 22:16:25 +0700

    menambahkan route filter

--- src/Pages/Katalog Pages/Filter/TabelFilter.jsx
+++ src/Pages/Katalog Pages/Filter/TabelFilter.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
-import './TabelKatalog.css';
+import './TabelFilter.css';
 import { Card, Space, Button } from 'antd';
 import { starfilled, truckIcon } from "../../../assets/Assets";
 import ChatButton from "../../../Komponen/ChatButton/ChatButton";
 import axios from 'axios';
+import { useLocation } from "react-router-dom";
 
-const TabelKatalog = () => {
+const TabelFilter = () => {
+    const location = useLocation();
+  const { state } = location || {};
+  const { origin, destination, tipeKendaraan, rekomendasi } = state || {};
   const [mappedData, setMappedData] = useState([]);
 
   useEffect(() => {
     const fetchCardData = async () => {
       try {
         const response = await axios.get(
-            `https://api-logee.vercel.app/catalogs`
+            `https://api-logee.vercel.app/catalogs?origin=${origin}&destination=${destination}&vehicleType=${tipeKendaraan}&isFrozen=${rekomendasi.radioOption1}&isMoreThanTons=${rekomendasi.radioOption2}&isMoreThan3Days=${rekomendasi.radioOption3}&isFragile=${rekomendasi.radioOption4}`
         );
         const data = response.data.map(item => ({
           id: item._id,
@@ -99,4 +103,4 @@ const TabelKatalog = () => {
   );
 };
 
-export default TabelKatalog;
+export default TabelFilter;
